09a8f7a32e2fe4003e44ea2afb8d7e6c367fd1c4
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-25 15:55:24 -0700

    Before dadtabase branch

---
+++ client/src/components/PriceVolumeChart.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class PriceVolumeChart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {    
+        } 
+    }
+    render(){
+        return (
+            <div>
+                <h1>HELLO FROM REACT</h1>
+            </div>
+        )
+    }
+}
+export default PriceVolumeChart;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,24 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-class PriceVolumeChart extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-          
-        }
-        
-    }
-    
-
-    render(){
-        return (
-            <div>
-                <h1>HELLO FROM REACT</h1>
-            </div>
-        )
-    }
-}
+import PriceVolumeChart from './components/PriceVolumeChart.jsx';
 
 ReactDOM.render(<PriceVolumeChart />, document.getElementById('priceVolumeChart'));
