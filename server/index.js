78a029b2b714d05c17c57677951632b5e20f0a0a
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-26 15:13:02 -0700

    Cleanup code

--- server/index.js
+++ server/index.js
@@ -10,21 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-
 app.get('/api/volumes/symbols/:symbolId', function(req, res){
   console.log(req.params.symbolId);
-  //res.end(JSON.stringify(res.));
   PriceVolume.find({_id: req.params.symbolId}, (err, data)=>{
     if(err) console.log(err);
-    console.log(data);
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(data));
   })
-
-
 })
-
-
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
