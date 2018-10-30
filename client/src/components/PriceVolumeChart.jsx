6efd4740e4b0c2fc4d888e327fde340d06159f18
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-29 18:45:43 -0700

    Add and edit values to display
    1. Add difference between current and average price in percentage with 'Higher' or 'Lower'
    
    2. Edit Average Price Paid value to be present up to 2 decimal points.

--- client/src/components/PriceVolumeChart.jsx
+++ client/src/components/PriceVolumeChart.jsx
@@ -12,7 +12,8 @@ class PriceVolumeChart extends React.Component {
             lowest: 0,
             heighest: 0,
             averagePrice: 0,
-            currentPrice: 0
+            currentPrice: 0,
+            difference:0
         } 
     }
     componentDidMount(){
@@ -22,6 +23,8 @@ class PriceVolumeChart extends React.Component {
     handleFetch(id = '5bd277855f59f4e021dd7a62'){
         $.get(`http://localhost:3002/api/volumes/symbols/${id}`, (data)=>{
             console.log(data[0]);
+            let diff = this.getDifference(data[0].averagePrice, data[0].currentPrice);
+
             this.setState({
                 symbol: data[0].symbol,
                 name: data[0].name,
@@ -30,21 +33,24 @@ class PriceVolumeChart extends React.Component {
                 lowest: data[0].lowest,
                 heighest: data[0].heighest,
                 averagePrice: data[0].averagePrice,
-                currentPrice: data[0].currentPrice
+                currentPrice: data[0].currentPrice,
+                difference: diff
             });
         })
     }
-
-
+    getDifference(averagePrice, currentPrice){
+        return Math.round(currentPrice/averagePrice*100-100);
+    }
     render(){
         return (
             <div>
                 <h1>Price Paid on Robinhood</h1>
-                <h2 id='name'>Name: {this.state.name}</h2>
-                <h2 id='lowest'>52 Week Low: ${this.state.lowest}</h2>
-                <h2 id='heighest'>52 Week High: ${this.state.heighest}</h2>
-                <h2 id='average'>Average Price Paid: ${this.state.averagePrice}</h2>
-                <h2 id='current'>Current Price: ${this.state.currentPrice}</h2>
+                <h2>Name: <span id='name'>{this.state.name}</span></h2>
+                <h2>52 Week Low: $<span id='lowest'>{this.state.lowest}</span></h2>
+                <h2>52 Week High: $<span id='heighest'>{this.state.heighest}</span></h2>
+                <h2>Average Price Paid: $<span id='average'>{this.state.averagePrice.toFixed(2)}</span></h2>
+                <h2>Current Price: $<span id='current'>{this.state.currentPrice}</span></h2>
+                <h2 id='difference'><span>{Math.abs(this.state.difference)}</span>% {this.state.difference>0 ? 'Higher':'Lower'}</h2>
             </div>
         )
     }
