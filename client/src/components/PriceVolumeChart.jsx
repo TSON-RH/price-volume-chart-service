1285d8ffa56fd7c0de184049637f3f35705c7584
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-10-30 20:52:15 -0700

    Link stylesheet

--- client/src/components/PriceVolumeChart.jsx
+++ client/src/components/PriceVolumeChart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
+import { runInThisContext } from 'vm';
 
 class PriceVolumeChart extends React.Component {
     constructor(props) {
@@ -45,12 +46,31 @@ class PriceVolumeChart extends React.Component {
         return (
             <div>
                 <h1>Price Paid on Robinhood</h1>
-                <h2>Name: <span id='name'>{this.state.name}</span></h2>
+                {/* <h2>Name: <span id='name'>{this.state.name}</span></h2>
                 <h2>52 Week Low: $<span id='lowest'>{this.state.lowest}</span></h2>
                 <h2>52 Week High: $<span id='heighest'>{this.state.heighest}</span></h2>
                 <h2>Average Price Paid: $<span id='average'>{this.state.averagePrice.toFixed(2)}</span></h2>
                 <h2>Current Price: $<span id='current'>{this.state.currentPrice}</span></h2>
-                <h2 id='difference'><span>{Math.abs(this.state.difference)}</span>% {this.state.difference>0 ? 'Higher':'Lower'}</h2>
+                <h2 id='difference'><span>{Math.abs(this.state.difference)}</span>% {this.state.difference>0 ? 'Higher':'Lower'}</h2> */}
+
+                <svg viewBox="0 0 400 200" width="400" height="200"xmlns="http://www.w3.org/2000/svg">
+                    <rect x="0" y="0" width="400" height="200" id="bargraph" fill="black">
+
+                    </rect>
+                    
+                    <g fill="white" >
+                    {
+                        this.state.volumes.map((height, i)=>{
+                            return(
+                            <rect x={30*i} y="1000" width="10" height={height}>
+                            
+                            </rect>)
+                        })
+                        
+                    }
+                    </g>
+                    
+                </svg>
             </div>
         )
     }
