2c5721e50114e64718ef8004a8fb8901b7e55859
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-11-02 20:16:54 -0700

    Add id to data schema

--- database/PriceVolume.js
+++ database/PriceVolume.js
@@ -3,6 +3,7 @@ const db = require('./index.js');
 mongoose.Promise = global.Promise;
 
 const priceVolumeSchema = new mongoose.Schema({
+    id: Number,
     symbol: String,
     name: String,
     prices: [Number],
