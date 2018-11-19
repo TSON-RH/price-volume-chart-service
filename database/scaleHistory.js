f59a4ebe6b41e950e001bc40454466e59b7031a6
Yuki Jung <yukiej@gmail.com>
2018-11-19 09:46:40 -0800

    Refactor stock history seeding script to use drain

--- database/scaleHistory.js
+++ database/scaleHistory.js
@@ -2,34 +2,57 @@ const fs = require('fs');
 const csvWriter = require('csv-write-stream');
 const faker = require('faker');
 
-const writer = csvWriter(
-  {sendHeaders: false}
-  );
+//wrapper for csvWriter to handle backpressure
+class Writer {
+  constructor(file) {
+    this.writer = csvWriter();
+    this.writer.pipe(fs.createWriteStream(file))
+  }
 
-const csvStream = fs.createWriteStream('./stockHistory.csv',
- {flags: 'a'}
- );
-writer.pipe(csvStream);
-
-const chunk = 1e6;
-  
-// for each stock
-for (let i = chunk*8; i < chunk*10; i += 1) {
-  //create 30 weeks for each stock
-  for (let week = 5; week < 10; week += 1) {
-    let price = parseFloat(faker.finance.amount(0.01, 110, 2));
-    let volume = Math.round(Math.random()*100);
-
-    // make the row
-    let newRow = {
-      id: i,
-      week: week,
-      price: price,
-      volume: volume,
+  write(obj) {
+    if (!this.writer.write(obj)) {
+      return new Promise(resolve => this.writer.once('drain', resolve))
     }
-    // write row to csv
-    writer.write(newRow);
-    }
+    return true;
   }
   
+  end() {
+    this.writer.end();
+  }
+}
+    
+
+
+// const csvStream = fs.createWriteStream('./stockHistory.csv',
+// //  {flags: 'a'}
+//  );
+// writer.pipe(csvStream);
+  
+(async() => {
+  const writer = new Writer('./stockHistory.csv');
+
+  // for each stock
+  for (let i = 0; i < 1e7; i += 1) {
+    //create 10 weeks for each stock
+    for (let week = 1; week < 10; week += 1) {
+      let price = parseFloat(faker.finance.amount(0.01, 110, 2));
+      let volume = Math.round(Math.random()*100);
+
+      // make the row
+      let newRow = {
+        id: i,
+        week: week,
+        price: price,
+        volume: volume,
+      }
+
+      // write row to csv
+      const res = writer.write(newRow);
+
+      if (res instanceof Promise) {
+        await res;
+      }
+    }    
+  }
   writer.end();
+})();
