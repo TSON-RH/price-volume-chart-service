810ce4f3aadde1e9c6bf5b6f7912797bee888531
Yuki Jung <yukiej@gmail.com>
2018-11-26 19:33:59 -0800

    Load tests for GET requests for microservice and proxy

---
+++ loadTests/PVbaseline_test.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export let options = {
+  vus: 10,
+  duration: "30s",
+  rps: 1000
+};
+
+export default function() {
+  let id = Math.floor(Math.random() * 1e7);
+  let res = http.get(`http://localhost:3002/api/volumes/symbols/${id}`);
+  check(res, {
+    "status was 200": (r) => r.status === 200
+  });
+};
